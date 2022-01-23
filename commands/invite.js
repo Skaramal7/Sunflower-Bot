ef3d4ef45c0740262a47c03b9ad8a2fec01cc303
Skaramal7 <skaramal7@gmail.com>
2022-01-23 17:00:31 -0300

    Fixing packages issues and better interaction

--- commands/invite.js
+++ commands/invite.js
@@ -1,3 +1,12 @@
+const { MessageButton } = require('discord-buttons');
+const { MessageActionRow } = require('discord-buttons');
+
 module.exports = async (client,msg) =>{
-    msg.channel.send("https://discord.com/oauth2/authorize?client_id=814667756087017493&scope=bot&permissions=8");
+
+let button = new MessageButton()
+  .setStyle('url')
+  .setURL('https://discord.com/oauth2/authorize?client_id=814667756087017493&scope=bot&permissions=8') 
+  .setLabel('Invite')
+
+    msg.channel.send("Click on the button to add the bot to your server!", button);
 }
